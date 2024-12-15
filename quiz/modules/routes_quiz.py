7da1643126a19157280ace67f297872cf0b52862
Michael McGarrah <mcgarrah@gmail.com>
2024-12-15 18:07:47 -0500

    Move everything around for the quiz.app naming

--- quiz/modules/routes_quiz.py
+++ quiz/modules/routes_quiz.py
@@ -5,7 +5,7 @@ import json
 import random
 from markupsafe import escape
 from flask import render_template, request, jsonify
-from modules.models import Question, Setting
+from quiz.modules.models import Question, Setting
 
 def quiz(category_id):
     """
